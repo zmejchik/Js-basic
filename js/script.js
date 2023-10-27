4988d9f2298becce7f4e1a7b0a4e26b20e739410
zmejchik <zmejchik@gmail.com>
2023-10-27 14:20:43 +0300

    JS basic

---
+++ js/script.js
@@ -0,0 +1,115 @@
+/**
+ * function delted div by id
+ */
+function deleteDiv() {
+  let divTask1 = document.getElementById("div-task1-1");
+  divTask1.remove();
+}
+/**
+ * function hidden div by id
+ */
+function deleteDivWithCSS() {
+  let divTask1 = document.getElementById("div-task1-1");
+  divTask1.className = "hidden";
+}
+/**
+ * function reverse style css display:hidden or display not hidden
+ */
+function reverseDisplayInSection1() {
+  let divTask1 = document.getElementById("div-task1-1");
+  if (divTask1.className == "hidden") {
+    divTask1.className = "";
+  } else {
+    divTask1.className = "hidden";
+  }
+}
+/**
+ * function reverse style css display:hidden or display not hidden
+ * with selected div by class
+ */
+function reverseDisplayFiveBlocks() {
+  let divTask3 = document.getElementsByClassName("divSection3");
+  for (let i = 0; i < divTask3.length; i++) {
+    if (divTask3[i].style.display == "none") {
+      divTask3[i].style.display = "";
+    } else {
+      divTask3[i].style.display = "none";
+    }
+  }
+}
+/**
+ * function reverse style css display:hidden or display not hidden
+ * with selected div by css selectors
+ */
+function reverseBlocksByCssSelector() {
+  let selector = document.getElementById("section4-input").value;
+  if (selector[0] == ".") {
+    let classSelectors = document.getElementsByClassName(selector.substring(1));
+    reverseHidden(classSelectors);
+  } else if (selector[0] == "#") {
+    let idSelector = selector.substring(1);
+    reverseDisplay(idSelector);
+  } else {
+    let tegSelectors = document.getElementsByTagName(selector);
+    reverseHidden(tegSelectors);
+  }
+}
+/**
+ * Function reverse hidden with selectors
+ */
+function reverseHidden(selectors) {
+  for (let i = 0; i < selectors.length; i++) {
+    if (selectors[i].style.display == "none") {
+      selectors[i].style.display = "";
+    } else {
+      selectors[i].style.display = "none";
+    }
+  }
+}
+/**
+ * Function reverse hidden with id
+ */
+function reverseDisplay(id) {
+  let element = document.getElementById(id);
+  if (element.style.display == "none") {
+    element.style.display = "";
+  } else {
+    element.style.display = "none";
+  }
+}
+/**
+ * Function view alert and change function for event onClick
+ */
+function onChangeFunction() {
+  alert("Привіт");
+  document.getElementById("section5").innerHTML =
+    '<div id="section5-div" onclick="removeSquare()"></div>';
+}
+function removeSquare() {
+  document.getElementById("section5").innerHTML = "";
+}
+/**
+ * Function visibility onmove mouse over button
+ */
+function reverseVisibility() {
+  let element = document.querySelector("#section6 div");
+  if (element.style.visibility == "visible") {
+    element.style.visibility = "hidden";
+  } else {
+    element.style.visibility = "visible";
+  }
+}
+/**
+ * Function visibility onfocus mouse on input
+ */
+function visibilityOnFokus() {
+  let element = document.querySelector("#section7 div");
+  element.style.visibility = "visible";
+}
+/**
+ * Function hidden onchange text on input
+ */
+function hiddenOnChange() {
+  let element = document.querySelector("#section7 div");
+  element.style.visibility = "hidden";
+}
